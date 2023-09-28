04d9026be8c05f81d61daa8a63fc0126a7e08314
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-09-28 23:51:44 +0900

    fix: remove server url environment value

--- src/lib/env.ts
+++
@@ -1,8 +0,0 @@
-if (!process.env.NEXT_PUBLIC_SERVER_URL)
-  throw new Error('Not found environment values')
-
-const env = {
-  NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-} as const
-
-export default env
